eb1d5e5012c66345fdb3c77f5956d05ec00dc422
Phan Văn Thanh <phanvanthanhda10tt@gmail.com>
2015-10-13 15:49:13 +0700

    fixed

--- public/js/custom/custom.js
+++ public/js/custom/custom.js
@@ -59,5 +59,10 @@ $(document).ready(function(){
         }
     });
 
+    // attach table filter plugin to inputs
+    $('[data-action="filter"]').filterTable();
+    $('[data-action="filter-2"]').filterTable();
+    
+
 });
 
